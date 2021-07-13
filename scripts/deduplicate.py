1c6c8d469b6d160e036a9aaa586dda4a8273ac4d
Ben Elsworth <be15516@bristol.ac.uk>
2021-07-13 23:17:50 +0100

    dbscan

--- scripts/deduplicate.py
+++ scripts/deduplicate.py
@@ -2,6 +2,9 @@ import pandas as pd
 import xarray as xr
 import numpy as np
 import os
+import sknetwork as skn
+from sklearn.cluster import DBSCAN
+
 from loguru import logger
 from vectology_functions import embed_text, create_aaa_distances
 
@@ -33,17 +36,31 @@ def encode_gwas(df):
 def run_aaa(df):
     dd = np.load(trait_npy)
     aaa = create_aaa_distances(dd)
+    # set nan to 0
+    where_are_NaNs = aaa[np.isnan(aaa)]=0
+    # check for nan
+    logger.info(np.any(np.isnan(aaa)))
     logger.info(len(aaa))
-    data = xr.DataArray(aaa,dims=("x","y"),coords={"x":df['id'],"y":df['id']})
-    data.attrs["long_name"] = 'traits'
-    data.x.attrs["units"]="cosine distance"
-    data.y.attrs["units"]="cosine distance"
-    logger.info(data)
-    logger.info(data.x.attrs)
+    logger.info(aaa.shape)
+    
+    # xarray
+    #data = xr.DataArray(aaa,dims=("x","y"),coords={"x":df['id'],"y":df['id']})
+    #data.attrs["long_name"] = 'traits'
+    #data.x.attrs["units"]="cosine distance"
+    #data.y.attrs["units"]="cosine distance"
+    #logger.info(data)
+    #logger.info(data.x.attrs)
+    return aaa
+
+def cluster(aaa):
+    clustering = DBSCAN(eps=10, min_samples=2).fit(aaa)
+    labels = clustering.labels_
+    logger.info(len(set(labels)))
 
 if __name__ == "__main__":
     df = read_traits()
     encode_gwas(df)
-    run_aaa(df)
+    aaa = run_aaa(df)
+    cluster(aaa)
 
 
